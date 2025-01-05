4ab98545fc7548bd51293ab2930fee44a9e80103
Ffr4a <francescoconte.2102@gmail.com>
2025-01-05 13:16:44 +0100

    Update DateServicePublisher.java

--- DateServicePublisher.java
+++ DateServicePublisher.java
@@ -1,8 +1,12 @@
-import javax.xml.ws.Endpoint;
+import jakarta.xml.ws.Endpoint;
 
 public class DateServicePublisher {
     public static void main(String[] args) {
-        Endpoint.publish("http://localhost:8080/DateService", new DateService());
-        System.out.println("Servizio avviato su http://localhost:8080/DateService");
+        try {
+            Endpoint.publish("http://localhost:8081/DateService", new DateService());
+            System.out.println("Servizio avviato su http://localhost:8081/DateService");
+        } catch (Exception e) {
+            System.err.println("Errore durante l'avvio del servizio: " + e.getMessage());
+        }
     }
 }
